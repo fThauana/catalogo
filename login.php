f18f80aa5b0df38d42f46c0aff31806c0f376bda
Thauana <fthauanavitoria@gmail.com>
2025-04-10 16:36:06 -0300

    Reposicionar formulário para exibir mensagem abaixo

--- login.php
+++ login.php
@@ -1,27 +1,27 @@
-<?php
-session_start();
-$usuarioCorreto = 'rush';
-$senhacorreta = '12345678';
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $usuario = $_POST['usuario'];
-    $senha = $_POST['senha'];
-
-    // Use o hash real gerado anteriormente
-    if ($usuario === $usuarioCorreto && $senha === $senhacorreta) {
-        $_SESSION['logado'] = true;
-        header('Location: protegido.php');
-        exit;
-    } else {
-        echo "Usuário ou senha inválidos.";
-    }
-
-    
-}
-?>
-
 <form method="post">
     <input type="text" name="usuario" placeholder="Usuário">
     <input type="password" name="senha" placeholder="Senha">
     <button type="submit">Entrar</button>
 </form>
+
+<?php
+    session_start();
+    $usuarioCorreto = 'rush';
+    $senhacorreta = '12345678';
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $usuario = $_POST['usuario'];
+        $senha = $_POST['senha'];
+
+        // Use o hash real gerado anteriormente
+        if ($usuario === $usuarioCorreto && $senha === $senhacorreta) {
+            $_SESSION['logado'] = true;
+            header('Location: protegido.php');
+            exit;
+        } else {
+            echo "Usuário ou senha inválidos.";
+        }
+    }
+?>
+
+
