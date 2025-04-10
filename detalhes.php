87dfb5188b412575a54feb110d7a6ebf328db657
Thauana <fthauanavitoria@gmail.com>
2025-04-10 16:36:03 -0300

    Adicionar botão de voltar para a página inicial

--- detalhes.php
+++ detalhes.php
@@ -4,10 +4,14 @@ $id = $_GET['id'] ?? null;
 
 foreach ($musicas as $musica) {
     if ($musica['id'] == $id) {
+        
         echo "<h1>{$musica['titulo']}</h1>";
         echo "<img src='{$musica['imagem']}' width='200'><br>";
         echo "<p>Categoria: {$musica['categoria']}</p>";
         echo "<p>{$musica['descricao']}</p>";
+        ?>
+        <a href="index.php">Voltar</a>
+        <?php 
         exit;
     }
 }
