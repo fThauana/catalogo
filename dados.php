056169212f474b5b2fd3df7cc12b503a09c7a6ea
Thauana <fthauanavitoria@gmail.com>
2025-04-10 16:36:01 -0300

    Atualizar músicas pré-definidas

--- dados.php
+++ dados.php
@@ -12,87 +12,32 @@ $musicasFixas = [
     [
         'id' => 2,
         'titulo' => 'St. Chroma',
-        'categoria' => 'Hip-Hop/rap',
+        'categoria' => 'Hip-Hop, rap',
         'imagem' => 'https://akamai.sscdn.co/uploadfile/letras/albuns/9/8/4/e/2420011730135018.jpg',
-        'descricao' => 'Música percursora do albúm "Crhomakopia" de Tyler, the Creator.'
+        'descricao' => 'Com produção vibrante e atmosfera cinematográfica, "ST. CHROMA" de Tyler, The Creator é uma viagem sonora ousada e cheia de estilo.'
     ],
     [
         'id' => 3,
-        'titulo' => 'St. Chroma',
-        'categoria' => 'Hip-Hop/rap',
-        'imagem' => 'https://akamai.sscdn.co/uploadfile/letras/albuns/9/8/4/e/2420011730135018.jpg',
-        'descricao' => 'Música percursora do albúm "Crhomakopia" de Tyler, the Creator.'
+        'titulo' => 'APT',
+        'categoria' => 'Pop, Pop coreano',
+        'imagem' => 'https://upload.wikimedia.org/wikipedia/pt/7/78/Ros%C3%A9_%26_Bruno_Mars_-_Apt.png',
+        'descricao' => 'Uma mistura envolvente de batidas modernas com letras introspectivas, APT é aquela faixa que te faz pensar e sentir ao mesmo tempo.'
     ],
     [
         'id' => 4,
-        'titulo' => 'St. Chroma',
-        'categoria' => 'Hip-Hop/rap',
-        'imagem' => 'https://akamai.sscdn.co/uploadfile/letras/albuns/9/8/4/e/2420011730135018.jpg',
-        'descricao' => 'Música percursora do albúm "Crhomakopia" de Tyler, the Creator.'
+        'titulo' => 'Abracadabra',
+        'categoria' => 'Dance-pop, Electropop',
+        'imagem' => 'https://marcapop.com.br/wp-content/uploads/2025/02/lady-gaga-mastercard.jpeg',
+        'descricao' => 'Um pop energético e misterioso, "Abracadabra" de Lady Gaga encanta com sua magia sonora e estilo ousado.'
     ],
     [
         'id' => 5,
-        'titulo' => 'St. Chroma',
-        'categoria' => 'Hip-Hop/rap',
-        'imagem' => 'https://akamai.sscdn.co/uploadfile/letras/albuns/9/8/4/e/2420011730135018.jpg',
-        'descricao' => 'Música percursora do albúm "Crhomakopia" de Tyler, the Creator.'
+        'titulo' => 'Oscar Winning Tears',
+        'categoria' => 'Jazz',
+        'imagem' => 'https://i.scdn.co/image/ab67616d0000b27394e5237ce925531dbb38e75f',
+        'descricao' => 'Com elegância e emoção, "Oscar Winning Tears" de RAYE entrega um jazz intenso que mistura vulnerabilidade e sofisticação.'
     ],
-    [
-        'id' => 6,
-        'titulo' => 'St. Chroma',
-        'categoria' => 'Hip-Hop/rap',
-        'imagem' => 'https://akamai.sscdn.co/uploadfile/letras/albuns/9/8/4/e/2420011730135018.jpg',
-        'descricao' => 'Música percursora do albúm "Crhomakopia" de Tyler, the Creator.'
-    ],
-    [
-        'id' => 7,
-        'titulo' => 'St. Chroma',
-        'categoria' => 'Hip-Hop/rap',
-        'imagem' => 'https://akamai.sscdn.co/uploadfile/letras/albuns/9/8/4/e/2420011730135018.jpg',
-        'descricao' => 'Música percursora do albúm "Crhomakopia" de Tyler, the Creator.'
-    ],
-    [
-        'id' => 8,
-        'titulo' => 'St. Chroma',
-        'categoria' => 'Hip-Hop/rap',
-        'imagem' => 'https://akamai.sscdn.co/uploadfile/letras/albuns/9/8/4/e/2420011730135018.jpg',
-        'descricao' => 'Música percursora do albúm "Crhomakopia" de Tyler, the Creator.'
-    ],
-    [
-        'id' => 9,
-        'titulo' => 'St. Chroma',
-        'categoria' => 'Hip-Hop/rap',
-        'imagem' => 'https://akamai.sscdn.co/uploadfile/letras/albuns/9/8/4/e/2420011730135018.jpg',
-        'descricao' => 'Música percursora do albúm "Crhomakopia" de Tyler, the Creator.'
-    ],
-    [
-        'id' => 10,
-        'titulo' => 'St. Chroma',
-        'categoria' => 'Hip-Hop/rap',
-        'imagem' => 'https://akamai.sscdn.co/uploadfile/letras/albuns/9/8/4/e/2420011730135018.jpg',
-        'descricao' => 'Música percursora do albúm "Crhomakopia" de Tyler, the Creator.'
-    ],
-    [
-        'id' => 11,
-        'titulo' => 'St. Chroma',
-        'categoria' => 'Hip-Hop/rap',
-        'imagem' => 'https://akamai.sscdn.co/uploadfile/letras/albuns/9/8/4/e/2420011730135018.jpg',
-        'descricao' => 'Música percursora do albúm "Crhomakopia" de Tyler, the Creator.'
-    ],
-    [
-        'id' => 12,
-        'titulo' => 'St. Chroma',
-        'categoria' => 'Hip-Hop/rap',
-        'imagem' => 'https://akamai.sscdn.co/uploadfile/letras/albuns/9/8/4/e/2420011730135018.jpg',
-        'descricao' => 'Música percursora do albúm "Crhomakopia" de Tyler, the Creator.'
-    ],
-    [
-        'id' => 13,
-        'titulo' => 'St. Chroma',
-        'categoria' => 'Hip-Hop/rap',
-        'imagem' => 'https://akamai.sscdn.co/uploadfile/letras/albuns/9/8/4/e/2420011730135018.jpg',
-        'descricao' => 'Música percursora do albúm "Crhomakopia" de Tyler, the Creator.'
-    ],
+    
 ];
 
 $musicasExtras = $_SESSION['musicasExtras'] ?? [];
